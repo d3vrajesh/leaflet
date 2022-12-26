7637e57769d1596287d5699504568d57a7d9010c
d3vrajesh <d3vrajesh@gmail.com>
2022-12-26 18:01:07 +0530

    js backup

--- index.js
+++ index.js
@@ -7,58 +7,6 @@ var openStreet = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 openStreet.addTo(map);
-/*
-// google street 
-var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-    maxZoom: 20,
-    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
-});
-googleStreets.addTo(map);
-
-
-*/
-//--Marker-- mention latLong and option --Refer-- https://leafletjs.com/reference.html#marker 
-//var agriculture = L.divIcon({ className: 'fa-solid fa-wheat' });
-/*iconUrl: '/icons/agri.png',
-iconSize: [20, 20]
- 
-/*iconAnchor: [22, 94],
-popupAnchor: [-3, -76],
-shadowUrl: 'my-icon-shadow.png',
-shadowSize: [68, 95],
-shadowAnchor: [22, 94] 
-});
-*/
-/*
-var singleMarker = L.marker([11.1271, 78.6569], { icon: agriculture });
-var popup = singleMarker.bindPopup('The Green Icon indicates the agriculture project').openPopup()
-popup.addTo(map)
-
-var education = L.divIcon({ className: 'fas fa-user-graduate' });
-
-var singleMarker = L.marker([28.6139, 77.2090], { icon: education });
-var popup = singleMarker.bindPopup('The Blue graduate Icon indicates the education project').openPopup()
-popup.addTo(map)
-
-var health = L.divIcon({ className: 'fas fa-heartbeat' });
-
-var singleMarker = L.marker([19.0760, 72.8777], { icon: health });
-var popup = singleMarker.bindPopup('The Blue graduate Icon indicates the education project').openPopup()
-popup.addTo(map)
-
- ----alternate option to add the marker and pop up bind 
-var popup = L.popup()
-    .setLatLng([11.1271, 78.6569])
-    .setContent('<p>Hello world!<br />This is a nice popup.</p>')
-    .openOn(map);
-
-    var popup = L.popup(latlng, {content: '<p>Hello world!<br />This is a nice popup.</p>')
-    .openOn(map);  
-
-    //how to add leaflet marker filter based on category in javascript?
-*/
-//--- Layer control 
-
 
 //--- collection -1 
 var collection1 = { "type": "FeatureCollection", "features": [{ "type": "Feature", "properties": { "description": "<p><img src='https://c4.wallpaperflare.com/wallpaper/586/603/742/minimalism-4k-for-mac-desktop-wallpaper-preview.jpg'><p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. </p> ", "marker-color": "#e90c0c", "marker-size": "medium" }, "geometry": { "coordinates": [76.8681636538007, 26.461561664701165], "type": "Point" } }, { "type": "Feature", "properties": { "description": "<p><img src='https://cdn.pixabay.com/photo/2022/09/30/14/30/lavender-7489530__340.jpg'><p> Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. </p>  ", "marker-color": "#e90c0c", "marker-size": "medium" }, "geometry": { "coordinates": [85.58166487534226, 24.973380738839353], "type": "Point" } }, { "type": "Feature", "properties": { "description": "<p><img src='https://c4.wallpaperflare.com/wallpaper/586/603/742/minimalism-4k-for-mac-desktop-wallpaper-preview.jpg'><p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. </p>  ", "marker-color": "#e90c0c", "marker-size": "medium" }, "geometry": { "coordinates": [77.17348370387981, 14.89272535365579], "type": "Point" } }] }
@@ -117,7 +65,6 @@ var collection2 = {
 }
 
 
-
 // map.removeLayer(singleMarker)
 function onEachFeature(feature, layer) {
   // does this feature have a property named popupContent?
@@ -171,4 +118,3 @@ L.control.watermark = function (opts) {
 }
 
 L.control.watermark({ position: 'bottomleft' }).addTo(map);
-
